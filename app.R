33f098de0e40b232f7b045a592d91dc62f524e3a
pnarasim <priya.narasimhan@gmail.com>
2020-03-02 22:17:27 +0800

    some more playing around with the code to learn R 1. the taxizeDBs.csv file is the master list. see the top column names. TBD: get the taxoncolumn based on which to merge the results for each DB 2. saved the api keys using setenv : TBD but need to find the way to save this permanently for the user in his .rprofile or some such 3. the merge of the results from each db happens based on taxoncolumn (specified in the csv) 4. uploadcsv/downloadcsv functions will be added soon. 5. the individual results from each db are added into a "lookupresults_dbname.csv" file now for me to understand the column names/etc
    TBDS:
    1. From olly : understand which DBs will need to included
    2. which search functions to use to access each DB
    3. which columns from each result to include
    4. need more data, access to more unit tests to understand error handling/corner cases. dont understand the errors well enough
    5. once some of the major functions are workign ok, maybe a quick rewrite to make it modular and enable extensions/more details/functionality from each DB

--- app.R
+++ app.R
@@ -7,77 +7,116 @@ ui <- fluidPage(
   titlePanel("Welcome to Taxize"),
   
   
-  sidebarLayout(position = "left",
+  sidebarLayout(
     sidebarPanel(
-      width="12",
-      fluidRow(
-        column(4, 
-          textAreaInput(inputId = "namesList", h3("Text input"), value="panthera", 
-          placeholder = "Enter your scientific names here...")
-        ),
-        column(3,
-          h3("Fuzzy Lookup"),
-          actionButton("fuzzyLookup", "Resolve Names")
-        ),
-        column(5,
-          radioButtons("dataSources", 
-            h3("Data Sources to lookup"), 
-            choiceNames = DBNames,
-            choiceValues = DBIndexes,
-            selected = 0
-          )
-        )
-      ),
-      fluidRow(
-        column(3,
-               h3("DB Lookup"),
-               actionButton("dbLookup", "DB Lookup")
-        )
-      )
+          textAreaInput(inputId = "names_list", "Text input", value="panthera", 
+          placeholder = "Enter your scientific names here..."),
+          #actionButton("upload_csv", "Upload CSV"),
+          actionButton("fuzzy_lookup", "Resolve Names"),
+          h3(""),
+          h3(""),
+          selectInput("data_sources", 
+            "Data Sources to lookup", 
+            choices = choicesDB,
+            multiple=TRUE,
+            selected = 1
+          ),
+          actionButton("db_lookup", "DB Lookup"),
+          h3(""),
+          actionButton("set_api_keys", "Set your API keys"),
+          h3(""),
+          conditionalPanel(condition = "input.set_api_keys == 0", 
+                           selectInput("api_keys", "Provide API Key for DB ", c(choicesAPIKeys)),
+                           textInput("api_key_value", "Enter the API Key here"),
+                           actionButton("save_api_key", "Save Key to my Env")
+          ),
+          
     ),
     mainPanel(
-      width="7",
-      fluidRow(
-        h3("Results of Lookup"),
-        tableOutput("lookupResultsTable")
-      ),
-      fluidRow(
-        h3("Resolved Names"),
-        tableOutput("resolvedNames")
-      )
-    ),
+        #h3("Results of Lookup"),
+        tableOutput("results_lookup_table"),
+        #h3("Resolved Names"),
+        tableOutput("resolved_names"),
+        
+        actionButton("download_csv", "Download Results in CSV"),
+     ),
   )
 )
 
 # Define server logic ----
 server <- function(input, output) {
+  ans = eventReactive(input$db_lookup, {
     
-  ans = eventReactive(input$dbLookup, {
-    speciesNames <- unlist(strsplit(input$namesList,"\n"))
-    #for loop if permitting lookup for multiple DBs and collating
-    #for (i in input$dataSources) {
-      cat(file=stderr(), " Selected source ", DBNames[as.integer(input$dataSources)], "Lookup function = " , DBLookupFunctions[as.integer(input$dataSources)], " \n")
+    species_names <- unlist(strsplit(input$names_list,"\n"))
+    
+    resolved_frames = gnr_resolve(name=species_names, 
+                                  best_match_only = TRUE,
+                                  canonical = TRUE,
+                                  fields = 'all', # Get all data fields, we can trim down after
+                      )
+    print_resolved_names(resolvedframes = resolved_frames)
+    master_lookup <- resolved_frames
+    
+    #for loop lookup for multiple DBs and collating
+    for (i in as.integer(input$data_sources)) {
       #check if API key required, if yes, if available
-      searchFunction = match.fun(DBLookupFunctions[as.integer(input$dataSources)])
+      
+      #searchFunction = match.fun(DBLookupFunctions[i])
+      #cat(file=stderr(), " \nSelected source ", DBNames[i], "Lookup function = " , DBLookupFunctions[i], " \n")      
+      
       #taxize call
-      do.call(searchFunction, list(speciesNames))
-    #}
+      lookups <- do.call(DBLookupFunctions[i], list(resolved_frames$matched_name2, DBLookupArgs[i]))
+      
+      #the merge will be with a different column name for each search? using from csv file for now
+      #tbd : rename columns in the merged list
+      master_lookup = merge(master_lookup, lookups, by.x="matched_name2", by.y=DBLookpupTaxonColumn[i]) 
+      
+      results_filename <- paste("lookupresults", DBNames[i], ".csv", sep="_")
+      cat(file=stderr(), " \nWriting results to file ", results_filename)
+      write.csv(lookups, results_filename)
+    }
+    #tbd : update ans properly with selected columns from all csv files
+    
+    ans <- master_lookup
   })
   
-  observeEvent(input$fuzzyLookup , {
-    speciesNames <- unlist(strsplit(input$namesList,"\n"))
-    resolved_frames = gnr_resolve(name=speciesNames)
-    output$resolvedNames <- renderTable({
+  observeEvent(input$fuzzy_lookup , {
+    species_names <- unlist(strsplit(input$names_list,"\n"))
+    resolved_frames = gnr_resolve(name=species_names, 
+                                  best_match_only = TRUE,
+                                  canonical = TRUE,
+                                  fields = 'all', # Get all data fields, we can trim down after
+                                  )
+    output$resolved_names <- renderTable({
       resolved_frames
     })
   })
   
-  observeEvent(input$dbLookup, {
-    output$lookupResultsTable <- renderTable({ 
+  observeEvent(input$db_lookup, {
+    output$results_lookup_table <- renderTable({ 
       ans()
     })
   })
+  
+  observeEvent(input$download_csv, {
+    write.csv(data_frames, "resolvednames.csv") 
+  })
+  
+  observeEvent(input$save_api_key, {
+    #tbd: set this perm in the .Rprofile or something
+    cat(file=stderr(), " \n api_keys = ", input$api_keys)
+    env_var = DBsNeedingAPIKeysEnvVar[as.integer(input$api_keys)]
+    cat(file=stderr(), "\nSetting env var", env_var, " with value ", input$api_key_value)
+    Sys.setenv(env_var=input$api_key_value)
+  })
 }
+
+print_resolved_names <- function(resolvedframes) {
+  for (j in NROW(resolvedframes)) {
+     cat(file=stderr(), " \nResolved names[", j, "] = ", resolvedframes$matched_name2[j])
+  }
+}
+
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
--- taxize_if.R
+++ taxize_if.R
@@ -7,3 +7,31 @@ DBNames <- c(taxizeDBs$Name)
 DBIndexes <- c(as.integer(taxizeDBs$Index))
 
 DBLookupFunctions <- c(taxizeDBs$LookupFunction)
+#DBLookupArgs <- c(taxizeDBs$ExtraArgs)
+DBLookpupTaxonColumn <- c(taxizeDBs$TaxonColumn)
+DBEnvVars <- c(taxizeDBs$EnvVar)
+choicesDB = setNames(DBIndexes, DBNames)
+
+DBAPIKeys <- c(as.integer(taxizeDBs$ApiKeyRequired))
+
+DBsNeedingAPIKeys <- c()
+DBsNeedingAPIKeysIndexes <- c()
+DBsNeedingAPIKeysEnvVar <- c()
+
+j <- 0
+for(i in DBIndexes) {
+  if (DBAPIKeys[i] != 0) {
+    j <- j+1
+    DBsNeedingAPIKeys[j] <- DBNames[i] 
+    DBsNeedingAPIKeysIndexes[j] <- j
+    DBsNeedingAPIKeysEnvVar[j] <- DBEnvVars[i]
+    cat(file=stderr(), " \nAdding DB ", DBNames[i], " with Env Var ", DBEnvVars[i], " j = ", j)
+  }
+}
+
+choicesAPIKeys = setNames(DBsNeedingAPIKeysIndexes, DBsNeedingAPIKeys)
+
+
+
+
+
